15f6e1af523d9410f351b6ed19b89519e0b7fdf4
Anton Romashkin <anton.v.romashkin@gmail.com>
2020-05-05 15:55:40 +0300

    отступы 2

--- lesson3/create_tables.sql
+++ lesson3/create_tables.sql
@@ -2,17 +2,17 @@ USE vk_new;
 
 DROP TABLE IF EXISTS attachments;
 CREATE TABLE attachments(
-	id SERIAL PRIMARY KEY,
-	media_id BIGINT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
+  media_id BIGINT UNSIGNED NOT NULL,
 
   FOREIGN KEY (media_id) REFERENCES media(id)
 );
 
 DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
-	id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   user_id BIGINT UNSIGNED NOT NULL,
-	attachment_id BIGINT UNSIGNED,
+  attachment_id BIGINT UNSIGNED,
   head VARCHAR(255),
   body TEXT,
   created_at DATETIME DEFAULT NOW(),
@@ -24,9 +24,9 @@ CREATE TABLE posts (
 
 DROP TABLE IF EXISTS media_communities;
 CREATE TABLE media_communities(
-	media_id BIGINT UNSIGNED NOT NULL,
-	community_id BIGINT UNSIGNED NOT NULL,
+  media_id BIGINT UNSIGNED NOT NULL,
+  community_id BIGINT UNSIGNED NOT NULL,
 
-	PRIMARY KEY (media_id, community_id),
+  PRIMARY KEY (media_id, community_id),
   FOREIGN KEY (community_id) REFERENCES communities(id)
 );
